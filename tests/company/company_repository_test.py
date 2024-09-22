9e17c0eec8c4fc5a421b1f40919ff0e634ecd0ce
Lukasz Zmudzinski <lukasz@zmudzinski.me>
2024-09-22 08:17:15 +0200

    Add company tests

---
+++ tests/company/company_repository_test.py
@@ -0,0 +1,17 @@
+import json
+
+
+class TestCompanyRepository:
+    def test_company_repository_constructor(self, company_repository):
+        data_path = "data/company.yml"
+        repository = company_repository(data_path=data_path)
+
+        assert repository._data_path == data_path
+    
+    def test_company_list_returns_correct_company_count(self, company_repository, company_data, mocker):
+        mocked_data = mocker.mock_open(read_data=json.dumps(company_data))
+        mocker.patch("builtins.open", mocked_data)
+
+        result = company_repository().get_companies()
+
+        assert len(result) == 2
---
+++ tests/company/conftest.py
@@ -0,0 +1,27 @@
+import pytest
+
+from company.repository import CompanyRepository
+
+
+@pytest.fixture
+def company_repository():
+    def _company_repository(data_path="dummy_path"):
+        return CompanyRepository(data_path=data_path)
+    
+    return _company_repository
+
+
+@pytest.fixture
+def company_data():
+    return {
+        "items": [
+            {
+                "name": "INPERIUM",
+                "icon": "inperium.svg",
+            },
+            {
+                "name": "Apple",
+                "icon": "apple.svg",
+            },
+        ],
+    }
